5616f93915b9b12a15a7cedb8d09d8378a936683
Nancy <31119437+NancyVazquez27@users.noreply.github.com>
2017-08-28 23:45:34 -0500

    Al principio ponia un solo estudiante en todas las posiciones, despues logre corregir eso y al mommento de querer eliminar un alumno lo hacia, pero despues al eliminar el otro me enviaba mensaje de error

--- Estudiante.java
+++ Estudiante.java
@@ -2,15 +2,17 @@ public class Estudiante
 {
     private String nombre;
     private int clave;
+    public int clave1;
     
     public Estudiante(int claveIni,String nombreIni)
     {
         nombre=nombreIni;
         clave=claveIni;
+        clave1=claveIni;
     }
     
     public String dimeDetalles()
     {
-        return(" Clave:"+clave+ "Nombre:"+nombre);
+        return(" Clave: "+clave+ " Nombre: "+nombre);
     }
 }
--- Grupo.java
+++ Grupo.java
@@ -2,24 +2,48 @@ public class Grupo
 {
     private String nomMateria;
     private Estudiante[] estudiantes;
+    private int numInscritos;
+    private int totalEs;
+  
     
     public Grupo(int totalEstudiantes,String nomMateria)
     {
         estudiantes=new Estudiante[totalEstudiantes];
         this.nomMateria=nomMateria;
+        numInscritos=0;
+        totalEs=totalEstudiantes;
+        
     }
     
     //Terminar este metodo de tarea
     
     public void inscribir(Estudiante unEstudiante)
     {
-        if(estudiantes[0]==null)
-        {
-            estudiantes[0]= unEstudiante;
-        }
         
+        if(numInscritos<totalEs)
+        {
+            estudiantes[numInscritos]=unEstudiante;
+            numInscritos++;
+        }
     }
     
-    //dar de baja un estudiante
-    // buscar el estudiante con la clave que te da & poner en null ese espacio
+   // public void darBaja(int claveBaja)
+    
+   public void baja(int claveBaja)
+   {
+       int i=0;
+       
+       if(claveBaja!=estudiantes[i].clave1)
+       {
+           i++;
+           
+        }
+        else
+           {
+           estudiantes[i]=null;
+           numInscritos=numInscritos-1;
+            }
+        }
+        //dar de baja un estudiante
+    // buscar el estudiante con la clave que te da & poner en null ese espacio  
 }
